eff6d25a1fed80572dcdfb2d2ce9a1024baa567a
Deil92 <khristoforov.vadim@yandex.ru>
2021-09-17 15:05:47 +0300

    Update link in header menu

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <v-container class="pt-8">
             <v-app-bar class="white elevation-15 rounded-lg px-2">
                 <v-app-bar-title>
-                    <router-link to="/?page=1" class="headline font-weight-bold text-decoration-none black--text">Цитаты</router-link>
+                    <h3 @click.prevent="toHome" class="to-home headline font-weight-bold text-decoration-none black--text">Цитаты</h3>
                 </v-app-bar-title>
                 <v-spacer></v-spacer>
                 <create-quote></create-quote>
@@ -21,11 +21,23 @@ export default {
     components: {
         CreateQuote
     },
+    methods: {
+        toHome() {
+            if(this.$route.path == '/'){
+                this.$store.dispatch('updateQuotes', 1);
+                return;
+            }
+            this.$router.push('/');
+        }
+    }
 }
 </script>
 
 <style>
 .v-card__title{
     word-break: break-word !important;
+}
+.to-home{
+    cursor: pointer;
 }
 </style>
